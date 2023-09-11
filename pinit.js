608a6b3eca4461b3c0e242bcc2407f689a341dea
Modarbro <121029406+Modarbro@users.noreply.github.com>
2023-09-12 04:01:33 +0700

    Update pinit.js

--- pinit.js
+++ pinit.js
@@ -1,1 +1,33 @@
-!function(a,b,c){var d,e,f;d="PIN_"+~~((new Date).getTime()/864e5),a[d]?a[d]+=1:(a[d]=1,a.setTimeout(function(){e=b.getElementsByTagName("SCRIPT")[0],f=b.createElement("SCRIPT"),f.type="text/javascript",f.async=!0,f.src=c.mainUrl+"?"+Math.random(),e.parentNode.insertBefore(f,e)},10))}(window,document,{mainUrl:"https://cdn.statically.io/gh/Modarbro/Pinterest/main/pinit_main.js"});
+/* jshint indent: false, maxlen: false */
+
+(function (w, d, a) {
+
+  var hazPinIt, firstScript, newScript, scriptUrl;
+
+  // generate an unique-ish global variable: hazPinIt_ plus today's Unix day
+  hazPinIt = 'PIN_' + ~~(new Date().getTime() / 86400000);
+
+  if (!w[hazPinIt]) {
+
+    // keep track of the number of times pinit.js is included for debugging purposes
+    w[hazPinIt] = 1;
+
+    // avoid KB927917 error in IE8
+    w.setTimeout(function () {
+
+      // load payload
+      firstScript = d.getElementsByTagName('SCRIPT')[0];
+      newScript = d.createElement('SCRIPT');
+      newScript.type = 'text/javascript';
+      newScript.async = true;
+      newScript.src = a.mainUrl + '?' + Math.random();
+      firstScript.parentNode.insertBefore(newScript, firstScript);
+
+    }, 10);
+
+  } else {
+    w[hazPinIt] += 1;
+  }
+}(window, document, {
+  'mainUrl': 'https://assets.pinterest.com/js/pinit_main.js'
+}));
